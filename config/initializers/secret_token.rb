f094ff9550d6ff8f8bff938bec0cb9b7d2090ea0
Umberto Di Tommaso <umbertoditommaso@gmail.com>
2011-12-18 15:47:09 +0100

    Primo commit con app vuota

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppProva::Application.config.secret_token = '903b1cf0ecfeedcfaa6fbf80702dabb4c15b78dee8dabd909e040d6b1003151dae4c38204f09f71d81d31b7517f16866ae9dbfd3ff5738772b86ba9321819a72'
